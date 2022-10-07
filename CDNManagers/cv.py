99d5e4fa254c4407ef3b559fee4a2748162ef7a7
Pranav C <xtreme5351@gmail.com>
2022-10-07 12:29:47 +0100

    debug version, still working on fix

--- CDNManagers/cv.py
+++ CDNManagers/cv.py
@@ -1,32 +1,45 @@
-from detector import Detector
+from CDNManagers import Detector
 import os
 
 
 class CV:
     def __init__(self):
-        pass
+        self.paths = self.resolve_files(True)
+        self.detector = Detector(self.paths[0], self.paths[1], self.paths[2], self.paths[3])
 
     @staticmethod
     def get_abs_file_paths(path):
         return os.path.abspath(path)
 
-    def start(self, vid_path=0):
-        config_path = self.get_abs_file_paths("CVDetector/data/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
-        model_path = self.get_abs_file_paths("CVDetector/data/frozen_inference_graph.pb")
-        # the above poos when this = os.path.join is used
-        #  Hard coded for now, just to make sure it works
-        classes_path = self.get_abs_file_paths("CVDetector/data/coco.names")
+    @staticmethod
+    def resolve_files(debug=False):
+        vid_path = 0
+        if not debug:
+            config_path = os.path.abspath("ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+            model_path = os.path.abspath("frozen_inference_graph.pb")
+            # the above absolutely shits itself
+            #  Use debug path for now
+            classes_path = os.path.abspath("coco.names")
+        else:
+            print("L tbh, using debug path")
+            # config_path = "/Users/pc/PycharmProjects/cdn_app/CDNManagers/data/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+            config_path = "/Users/pc/PycharmProjects/cdn_app/CDNManagers/data/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+            model_path = "/Users/pc/PycharmProjects/cdn_app/CDNManagers/data/frozen_inference_graph.pb"
+            # the above poos when this = os.path.join is used
+            #  Hard coded for now, just to make sure it works
+            classes_path = "/Users/pc/PycharmProjects/cdn_app/CDNManagers/data/coco.names"
+        return vid_path, config_path, model_path, classes_path
 
-        detector = Detector(vid_path, config_path, model_path, classes_path)
-        detector.onVideo()
+    def start(self):
+        self.detector.onVideo()
 
 
 def main():
     # vid path 0 represents webcam
     vid_path = 0
-    config_path = r"C:\Users\Jonathan\OneDrive\CDN\CDN-App\CDNManagers\data\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
-    model_path = r"C:\Users\Jonathan\OneDrive\CDN\CDN-App\CDNManagers\data\frozen_inference_graph.pb"
-    classes_path = r"C:\Users\Jonathan\OneDrive\CDN\CDN-App\CDNManagers\data\coco.names"
+    config_path = os.path.join("data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+    model_path = os.path.join("data", "frozen_inference_graph.pb")
+    classes_path = os.path.join("data", "coco.names")
 
     detector = Detector(vid_path, config_path, model_path, classes_path)
     detector.onVideo()
