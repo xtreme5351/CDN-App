a622868fbebc7570cb6fa18aa98e019159584d7b
Pranav C <xtreme5351@gmail.com>
2022-11-25 14:45:12 +0000

    basic connection working

--- CDNManagers/connection.py
+++ CDNManagers/connection.py
@@ -2,7 +2,31 @@
 # This is the client, the RPI is the server
 
 import socket
+import fcntl
+import struct
+from netifaces import AF_INET, ifaddresses
+
 
 class Connect:
+    def __init__(self):
+        self.PORT = 65432
+        self.protocol = 'eth0'
+        self.HOST = ""
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.get_rpi_ip()
+
+    def get_rpi_ip(self):
+        self.HOST = ifaddresses('en0')[AF_INET]
+        print(self.HOST)
+
     def start_connection(self):
         print("Connecting")
+        with self.sock as s:
+            s.connect((self.HOST, self.PORT))
+            s.sendall(b"Cum")
+            data = s.recv(1024)
+        print(f"Received {data!r}")
+
+
+if __name__ == "__main__":
+    Connect().start_connection()
--- RPI/connector.py
+++ RPI/connector.py
@@ -2,3 +2,19 @@
 # In theory, the daemon constantly listens for connections and when one is found, blocks all other
 # Should establish a permanent link until client-side termination
 
+import socket
+
+HOST = "169.254.66.159"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
